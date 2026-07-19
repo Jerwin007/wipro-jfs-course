7613099c6e66304441fb259d80ab738516cb7f88
JERWIN <153886797+Jerwin007@users.noreply.github.com>
2026-07-19 22:27:32 +0530

    Add files via upload

---
+++ wipro-jfs-course/LogicBuilding/src/L1/A2.java
@@ -0,0 +1,18 @@
+package L1;
+import java.util.Scanner;
+public class A2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("URK23CS1064");
+        int count = 0;
+        System.out.println("Enter marks of 3 students:");
+        for (int i = 1; i <= 3; i++) {
+            int mark = sc.nextInt();
+            if (mark >= 35) {
+                count++;
+            }
+        }
+        System.out.println("Students Passed = " + count);
+        sc.close();
+    }
+}
