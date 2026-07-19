1436d62eddae5aca4c3e72b4c03b0423d23eb5bd
JERWIN <153886797+Jerwin007@users.noreply.github.com>
2026-07-19 22:49:06 +0530

    Refactor A5.java and add LC5_1 for array merging

--- wipro-jfs-course/LogicBuilding/src/L1/A5.java
+++ wipro-jfs-course/LogicBuilding/src/L1/A5.java
@@ -1,24 +1,30 @@
-package L1;
-
-import java.util.Scanner;
-
-public class A5 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter first number: ");
-        int a = sc.nextInt();
-
-        System.out.print("Enter second number: ");
-        int b = sc.nextInt();
-
-        System.out.print("Enter third number: ");
-        int c = sc.nextInt();
-
-        int sum = a + b + c;
-
-        System.out.println("Sum = " + sum);
-
-        sc.close();
-    }
+package lb;
+import java.util.*;
+public class LC5_1 {
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		int n1, n2;
+        System.out.println("URK23CS1064");
+		System.out.print("Number of elements in Arr1: ");
+		n1=s.nextInt();
+		System.out.print("Number of elements in Arr2: ");
+		n2=s.nextInt();
+		int arr1[]=new int[n1];
+		int arr2[]=new int[n2];
+		for(int i=0; i<n1;i++) {
+			arr1[i]=s.nextInt();
+		}
+		for(int j=0; j<n2;j++) {
+			arr2[j]=s.nextInt();
+		}
+		int ans[]=new int[n1+n2];
+		for(int i=0; i<n1;i++) {
+			ans[i]=arr1[i];
+		}
+		for(int j=0; j<n2;j++) {
+			ans[n1+j]=arr2[j];
+		}
+		Arrays.sort(ans);
+		System.out.print("The sorted and merged array: "+Arrays.toString(ans));
+	}
 }
