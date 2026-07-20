3163c60e235b4d5a86f6c6fe47aab784a94a9949
JERWIN <153886797+Jerwin007@users.noreply.github.com>
2026-07-20 21:20:31 +0530

    Update A2.java

--- wipro-jfs-course/LogicBuilding/src/L1/A2.java
+++ wipro-jfs-course/LogicBuilding/src/L1/A2.java
@@ -2,14 +2,25 @@ package L1;
 import java.util.Scanner;
 public class A2 {
     public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-        System.out.print("URK23CS1064");
-		System.out.print("Enter the Number: ");
-		int n=s.nextInt();
-		while (n>0) {
-			int d=n%10;
-			System.out.print(d);
-			n=n/10;			
-		}
+        Scanner sc = new Scanner(System.in);
+        System.out.println("URK23CS1064");
+        System.out.print("Enter the Number: ");
+        int n = sc.nextInt();
+        int temp = n;
+        int count = 0;
+        while (temp > 0) {
+            count++;
+            temp = temp / 10;
+        }
+        int[] digits = new int[count];
+        for (int i = count - 1; i >= 0; i--) {
+            digits[i] = n % 10;
+            n = n / 10;
+        }
+        System.out.println("Digits are:");
+        for (int i = 0; i < count; i++) {
+            System.out.println(digits[i]);
+        }
+        sc.close();
     }
 }
